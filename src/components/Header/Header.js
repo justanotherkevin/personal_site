7256baa2ae60e3d2149528f730c156deee931b2e
Kevin Hu <hu.kevin.w@gmail.com>
2018-08-25 12:17:37 -0400

    New edits for header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      // <div className="header-wrapper">
+      //   <div className="header-content">
+      //     <h3>You name here</h3>
+      //     <hr class="star-light" />
+      //     <p>this is some text</p>
+      //   </div>
+      // </div>
+      <div>
+        
+      </div>
+    );
+  }
+}
