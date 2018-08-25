6e9a4300b02c8e26ce0eca0c6aa69dff9eb7647b
Kevin Hu <hu.kevin.w@gmail.com>
2018-08-25 12:14:06 -0400

    Modify navbar component -new

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,49 +1,62 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { stat } from 'fs';
 
 export default class Navbar extends Component {
-  handleClick = e => {
-    document.getElementById('navbarNav').classList.toggle('show');
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: 0
+    };
+  }
+  handleClick = e => {};
+  handelMenuClick = e => {
+    this.setState({ isMenuOpen: this.state.isMenuOpen === 0 ? 1 : 0 });
   };
   render() {
+    const toggleShow = this.state.isMenuOpen === 1 ? ' show' : '';
     return (
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={this.handleClick}
+      <header>
+        <div
+          className={
+            'menu-btn' + (this.state.isMenuOpen === 0 ? ' open' : ' close')
+          }
+          onClick={this.handelMenuClick}
+          ref="nav-menu-btn"
         >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="navbar-wrapper">
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-          </div>
+          <div className="btn-line" />
+          <div className="btn-line" />
+          <div className="btn-line" />
         </div>
-      </nav>
+
+        <nav className={'menu' + toggleShow}>
+          <div className={'menu-branding' + toggleShow}>
+            <div className="portrait" />
+          </div>
+          <ul className="menu-nav">
+            <li className="nav-item current">
+              <a href="index.html" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="about.html" className="nav-link">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="work.html" className="nav-link">
+                My Work
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="contact.html" className="nav-link">
+                How To Reach Me
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
