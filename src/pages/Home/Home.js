39ba70c4166a303f8b6e3195974c920ee0de5e95
Kevin Hu <hu.kevin.w@gmail.com>
2018-08-28 20:44:14 -0400

    Removed unused code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import Socials from '../../components/Socials/Socials';
-const card = () => (
-  <div className="card-wrapper">
-    <div className="img-wrapper" />
-    <div className="content-wrapper">
-      <h3>this header</h3>
-      <p> this is not the header</p>
-    </div>
-    <div className="action-wrapper">
-      <a href="#">link</a>
-    </div>
-  </div>
-);
+
 export default class Home extends Component {
   render() {
     return (
